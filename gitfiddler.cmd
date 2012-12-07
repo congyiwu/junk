2204109813aebddc74e6b35f39466ddfc304b889
Congyi Wu <congyiwu@gmail.com>
2012-12-07 17:53:03 -0500

    Add gitfiddler.cmd and shortcut.example.cmd

---
+++ gitfiddler.cmd
@@ -0,0 +1,36 @@
+@echo off 
+
+if [%1] equ [1] (
+  call :enable
+  goto :eof
+)
+
+if [%1] equ [0] (
+  call :disable
+  goto :eof
+)
+
+call :help
+goto :eof
+
+:enable
+  echo on
+  git config --global http.proxy localhost:8888
+  git config --global http.sslVerify 0
+  netsh winhttp set proxy localhost:8888
+  @echo off 
+goto :eof
+
+:disable
+  echo on
+  git config --global --unset http.proxy
+  git config --global --unset http.sslVerify
+  netsh winhttp reset proxy
+  @echo off 
+goto :eof
+
+:help
+  echo USAGE: gitfiddler.cmd 1^|0
+  echo 1: Set git config and winhttp to use proxy
+  echo 2: disable proxy for git config and winhttp
+goto :eof
---
+++ shortcut.example.cmd
@@ -0,0 +1,2 @@
+@echo off
+"C:\PATH\TO\bla.exe" %*
