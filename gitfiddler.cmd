0ddb205b5ca86a37baa65c0f60be3614978fc47f
Congyi Wu <congyiwu@gmail.com>
2014-11-21 15:07:23 -0500

    gitfiddler: don't touch winhttp settings

--- gitfiddler.cmd
+++ gitfiddler.cmd
@@ -24,7 +24,6 @@ goto :eof
   echo on
   git config --global http.proxy localhost:8888
   git config --global http.sslVerify 0
-  netsh winhttp set proxy localhost:8888
   @echo off 
 goto :eof
 
@@ -32,13 +31,12 @@ goto :eof
   echo on
   git config --global --unset http.proxy
   git config --global --unset http.sslVerify
-  netsh winhttp reset proxy
   @echo off 
 goto :eof
 
 :help
   echo USAGE: gitfiddler.cmd [1^|0]
-  echo 1: sets proxy to localhost:8888 in git config and winhttp
-  echo 0: unsets proxy in git config and winhttp
+  echo 1: sets proxy to localhost:8888 in git config
+  echo 0: unsets proxy in git config
   echo no params: sets, pauses, then unsets
 goto :eof
